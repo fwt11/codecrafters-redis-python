426f0fb5dbf6a0c6d60f03ad2379ee4046ec0b84
fwt11 <wutong.feng@139.com>
2023-03-23 21:05:41 +0800

    handle client

--- app/main.py
+++ app/main.py
@@ -9,12 +9,33 @@ def main():
     # Uncomment this to pass the first stage
     #
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    (conn, address) = server_socket.accept() # wait for client
+    server_socket.setblocking(False)
     
+    clients = []
+
     while True:
-        buf = conn.recv(32)
-        buf = buf.split(b'\r\n')
+        try:
+            conn, addr = server_socket.accept() # wait for client
+            conn.setblocking(False)
+            clients.append(conn)
+        except BlockingIOError as e:
+            pass
+
+        for client in clients:
+            handle_client(client)
+    
+
+def handle_client(conn):
+    try:
+        buff, addr = conn.recvfrom(32)
+        if not buff:
+            print(f"no more data, close client from {addr}")
+            return
         conn.sendall(b'+PONG\r\n')
+    except BlockingIOError as e:
+        return
+
+
 
 
 
